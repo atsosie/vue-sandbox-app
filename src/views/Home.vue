a1148d42b5141b13dd5345e68c6303caa755c709
Amber Tsosie <atxambra@gmail.com>
2019-05-21 09:34:53 -0700

    Added 'plans' component to home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,14 @@
   <span>
     <home-hero></home-hero>
     <home-details></home-details>
+    <home-plans></home-plans>
   </span>
 </template>
 
 <script>
 import HomeHero from '@/components/HomeHero';
 import HomeDetails from '@/components/HomeDetails';
+import HomePlans from '@/components/HomePlans';
 
 export default {
   name: 'Home',
@@ -15,6 +17,7 @@ export default {
   components: {
     HomeHero,
     HomeDetails,
+    HomePlans,
   },
 };
 </script>
